f4fa9ae11838fc6110c07d76b4acc281be711bb4
Jonathan Brown <jbrown@acuity.social>
2023-05-29 03:05:48 +0700

    Index fast_unstake more.

--- src/pallets/fast_unstake.rs
+++ src/pallets/fast_unstake.rs
@@ -13,6 +13,13 @@ pub fn fast_unstake_index_event(trees: Trees, block_number: u32, event_index: u3
             index_event_account_id(trees, event.stash, block_number, event_index);
             Ok(())
         },
+        "BatchChecked" => {
+            let event = event.as_event::<polkadot::fast_unstake::events::BatchChecked>()?.unwrap();
+            for era in event.eras {
+                index_event_era_index(trees.clone(), era, block_number, event_index);
+            }
+            Ok(())
+        },
         _ => Ok(()),
     }
 }
