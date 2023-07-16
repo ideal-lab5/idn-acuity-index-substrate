26329a10e835a69f90d0421c02c2998bb5425040
Jonathan Brown <jbrown@acuity.social>
2023-07-16 11:30:25 +0700

    Implement index_preimage_event!

--- src/substrate_pallets.rs
+++ src/substrate_pallets.rs
@@ -12,3 +12,21 @@ macro_rules! index_system_event {
         }
     };
 }
+
+#[macro_export]
+macro_rules! index_preimage_event {
+    ($event_enum: ty, $event: ident, $indexer: ident, $block_number: ident, $event_index: ident) => {
+        match $event {
+            <$event_enum>::Noted { hash } => {
+                $indexer.index_event_preimage_hash(hash.into(), $block_number, $event_index);
+            }
+            <$event_enum>::Requested { hash } => {
+                $indexer.index_event_preimage_hash(hash.into(), $block_number, $event_index);
+            }
+            <$event_enum>::Cleared { hash } => {
+                $indexer.index_event_preimage_hash(hash.into(), $block_number, $event_index);
+            }
+            _ => {}
+        }
+    };
+}
