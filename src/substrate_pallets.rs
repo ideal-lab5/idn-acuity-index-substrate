aea0c286bc3bd57b5410685bff28efb2692e172a
Jonathan Brown <jbrown@acuity.social>
2023-07-16 11:08:33 +0700

    Use catch-all for events that are not indexed.

--- src/substrate_pallets.rs
+++ src/substrate_pallets.rs
@@ -2,16 +2,13 @@
 macro_rules! index_system_event {
     ($event_enum: ty, $event: ident, $indexer: ident, $block_number: ident, $event_index: ident) => {
         match $event {
-            <$event_enum>::ExtrinsicSuccess { .. } => {}
-            <$event_enum>::ExtrinsicFailed { .. } => {}
-            <$event_enum>::CodeUpdated {} => {}
             <$event_enum>::NewAccount { account } => {
                 $indexer.index_event_account_id(account, $block_number, $event_index);
             }
             <$event_enum>::KilledAccount { account } => {
                 $indexer.index_event_account_id(account, $block_number, $event_index);
             }
-            <$event_enum>::Remarked { sender, .. } => {}
+            _ => {}
         }
     };
 }
