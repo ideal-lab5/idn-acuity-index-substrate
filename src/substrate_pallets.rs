80cf6471f36cfd9765f3b72d6ef3a8fa8f103c60
Jonathan Brown <jbrown@acuity.social>
2023-07-16 11:50:38 +0700

    Implement index_indicies_event!

--- src/substrate_pallets.rs
+++ src/substrate_pallets.rs
@@ -30,3 +30,23 @@ macro_rules! index_preimage_event {
         }
     };
 }
+
+#[macro_export]
+macro_rules! index_indicies_event {
+    ($event_enum: ty, $event: ident, $indexer: ident, $block_number: ident, $event_index: ident) => {
+        match $event {
+            <$event_enum>::IndexAssigned { who, index } => {
+                $indexer.index_event_account_id(who, $block_number, $event_index);
+                $indexer.index_event_account_index(index, $block_number, $event_index);
+            }
+            <$event_enum>::IndexFreed { index } => {
+                $indexer.index_event_account_index(index, $block_number, $event_index);
+            }
+            <$event_enum>::IndexFrozen { index, who } => {
+                $indexer.index_event_account_index(index, $block_number, $event_index);
+                $indexer.index_event_account_id(who, $block_number, $event_index);
+            }
+            _ => {}
+        }
+    };
+}
