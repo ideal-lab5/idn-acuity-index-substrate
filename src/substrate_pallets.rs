ddfaddf8d79e3dabcdaec8bddbd41681a80a9949
Jonathan Brown <jbrown@acuity.social>
2023-07-17 13:20:11 +0700

    Implement index_bags_list_event!

--- src/substrate_pallets.rs
+++ src/substrate_pallets.rs
@@ -686,3 +686,18 @@ macro_rules! index_child_bounties_event {
         }
     };
 }
+
+#[macro_export]
+macro_rules! index_bags_list_event {
+    ($event_enum: ty, $event: ident, $indexer: ident, $block_number: ident, $event_index: ident) => {
+        match $event {
+            <$event_enum>::Rebagged { who, .. } => {
+                $indexer.index_event_account_id(who, $block_number, $event_index);
+            }
+            <$event_enum>::ScoreUpdated { who, .. } => {
+                $indexer.index_event_account_id(who, $block_number, $event_index);
+            }
+            _ => {}
+        }
+    };
+}
